a0a671767c2d4009d1a61df52ef8da922ebb6b60
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-12-14 16:28:36 -0800

    lol

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,96 @@
 import { load } from "cheerio"
 import { prompt } from "enquirer"
 import { readFile, readdir } from "fs/promises"
-import render from "node-html-to-image"
 import { basename, resolve } from "path"
 
-import extract from "./lib/extract"
-import transform from "./lib/transform"
-import apply from "./lib/apply"
+import extract from "./lib/helpers/extract"
+import render from "./lib/render"
 
-readdir(`${__dirname}/../banners`).then(async (templates) => {
-    let selected_template = await prompt({
+readdir(`${__dirname}/../presets`).then(async (projectList) => {
+
+   let preset = (await prompt({
         type: "select",
-        name: "template",
-        message: "Select a template",
-        choices: templates
-    }) as {template: string}
+        name: "preset",
+        message: "Select a project to render the assets for...",
+        choices: [
+            ...projectList,
+            "Do not use a preset"
+        ],
+    }) as { preset: string }).preset
 
-    let page = (await readFile(`${__dirname}/../banners/${selected_template.template}`)).toString()
-    let $ = load(page);
+    if (preset == "Do not use a preset") {
 
-    let base = extract($);
+        // get a list of templates and prompt the user
+        let templates = await readdir(`${__dirname}/../banners`)
 
-    let inputs = (await prompt({
-        type: "form",
-        name: "fillin",
-        message: "Please set the following variables.",
-        choices: base.variables.map(v => {
-            return {
-                name: v.name,
-                message: `${v.name}${v.required ? "*" : ""}`
+        let template = (await prompt({
+            type: "select",
+            name: "template",
+            message: "Select a template",
+            choices: templates.map(e => basename(e, ".html"))
+        }) as {template: string}).template
+    
+        // extract 
+        let page = (await readFile(`${__dirname}/../banners/${template}.html`)).toString()
+        let $ = load(page);
+        let base = extract($);
+
+        let { inputs, output } = (await prompt([
+            {
+                type: "form",
+                name: "inputs",
+                message: "Please set the following variables.",
+                choices: base.variables.map(v => {
+                    return {
+                        name: v.name,
+                        message: `${v.name}${v.required ? "*" : ""}`
+                    }
+                })
+            },
+            {
+                type: "input",
+                name: "output",
+                message: "Where would you like to save the render?",
+                initial: resolve(`${__dirname}/../renders/${template}.png`)
             }
+        ]) as { inputs: { [key: string]: string }, output: string })
+
+        await render(template, inputs, output, $)
+        console.log(`Render saved to ${output}`)
+    } else {
+
+        // alright, let's start rendering everything in that preset
+
+        let templatePresets = 
+            await Promise.all(
+                (await readdir(`${__dirname}/../presets/${preset}/variables`))
+                .map(async (filename) => {
+                    return {
+                        template: basename(filename, ".json"),
+                        inputs: Object.fromEntries(
+                            Object.entries(
+                                JSON.parse(
+                                    (await readFile(`${__dirname}/../presets/${preset}/variables/${filename}`)).toString()
+                                ) as { [key: string]: string }
+                            ).map(([key, value]) => { 
+                                return [ key, 
+                                    value.startsWith("[ASSET] ")
+                                    // Resolve "[ASSET] abc.xyz" to "./presets/preset/assets/abc.xyz" 
+                                    ? `${__dirname}/../presets/${preset}/assets/${value.match(/\[ASSET\] (.*)/)?.[1]}`
+                                    : value 
+                                ] 
+                            })
+                        )
+                    }
+                })
+            )
+
+        templatePresets.forEach(({template, inputs}, x) => {
+            let outDir = resolve(`${__dirname}/../renders/${preset}-${template}.png`)
+            render(template, inputs, outDir)
+            console.log(`${x}/${templatePresets.length}: Rendered to ${outDir}`)
         })
-    }) as { fillin: { [key: string]: string } }).fillin
 
-    let applications = (await Promise.all(base.variables.map(async v => {
-        if (!inputs[v.name]) {
-            if (v.required) throw `did not provide required value ${v.name}`
-            else return
-        }
-
-        let value: string
-        if (v.transformation) value = await transform(v.transformation, inputs[v.name])!
-        else value = inputs[v.name]
-
-        return [ v.target, value ]
-    }))).filter((e): e is string[] => Boolean(e))
-
-    apply($, Object.fromEntries(applications))
-
-    let output = (await prompt({
-        type: "input",
-        name: "output",
-        message: "Where would you like to save the render?",
-        initial: resolve(`${__dirname}/../renders/${basename(selected_template.template, ".html")}.png`)
-    }) as { output: string } ).output
-
-    await render({
-        output,
-        html: $.html()
-    })
+    }
 
 })
---
+++ src/lib/render.ts
@@ -0,0 +1,43 @@
+import { type CheerioAPI, load } from "cheerio"
+import { readFile, readdir } from "fs/promises"
+import renderHtml from "node-html-to-image"
+
+import extract from "./helpers/extract"
+import transform from "./helpers/transform"
+import apply from "./helpers/apply"
+
+export default async function render(
+    template: string,
+    inputs: { [key: string]: string },
+    output?: string,
+    $?: CheerioAPI
+) {
+    // if not already loaded, let's load in the template
+    if (!$)
+        $ = load(
+            (await readFile(`${__dirname}/../../banners/${template}.html`))
+                .toString()
+        )
+
+    let base = extract($);
+
+    let applications = (await Promise.all(base.variables.map(async v => {
+        if (!inputs[v.name]) {
+            if (v.required) throw `did not provide required value ${v.name}`
+            else return
+        }
+
+        let value: string
+        if (v.transformation) value = await transform(v.transformation, inputs[v.name])!
+        else value = inputs[v.name]
+
+        return [ v.target, value ]
+    }))).filter((e): e is string[] => Boolean(e))
+
+    apply($, Object.fromEntries(applications))
+
+    return await renderHtml({
+        output,
+        html: $.html()
+    })
+}
